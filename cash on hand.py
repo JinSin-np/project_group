efb8207b12817ff4ded6e3f2ac37b8f313124158
Tay Jia Wei <s10262468@connect.np.edu.sg>
2023-07-26 18:36:50 +0800

    taken from CSV and changed

--- cash on hand.py
+++ cash on hand.py
@@ -1,2 +1,20 @@
-print("Hello World")
-print("Hi")
+from pathlib import Path
+import csv
+
+# create a file to csv file.
+fp = Path.cwd()/"COH & P&L.csv"
+
+# read the csv file to append profit and quantity from the csv.
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
+
+    # create an empty lists to store time sheet and overhead record
+    Cash_on_Hand=[]
+
+    # append time sheet and sales record into the overheadRecords list
+    for row in reader:
+        #get the day, items and amount for each record
+        #and append the salesRecords list
+        Cash_on_Hand.append([row[0],row[1],row[3]])
+print(Cash_on_Hand)

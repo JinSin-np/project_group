23bc2f2d4a382eb43464d63b7ed0e1808e72b3ac
Tay Jia Wei <s10262468@connect.np.edu.sg>
2023-07-28 00:17:11 +0800

    importing csv

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,2 +1,17 @@
-print("Hello World")
-print("Hi")
+from pathlib import Path
+import csv
+
+# create a file to csv file.
+fp = Path.cwd()/"COH & P&L.csv"
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
+
+    # create an empty lists to store time sheet and Profit and Loss record
+    Cash_on_hand = [] 
+
+    # append time sheet and profit and loss record into the PandL_Records list
+    for row in reader:
+        #get the day, items and profit for each record
+        #and append the salesRecords list
+        Cash_on_hand.append([row[0],int(row[4])])

c5c620cd22521c3d62bd297e2872a265e1f4d439
Tay Jia Wei <s10262468@connect.np.edu.sg>
2023-08-01 00:37:06 +0800

    trying but dying

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import csv
 
+
 # create a file to csv file.
 fp = Path.cwd()/"COH.csv"
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
@@ -16,79 +17,43 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #and append the salesRecords list
         Cash_on_hand.append([row[0],int(row[4])])
 
-# Convert the amount data to a list of integers
-amounts = [int(item[1]) for item in Cash_on_hand]
-
-increasing_period = []
-decreasing_period = []
-
-# Check for increasing and decreasing periods
-current_trend = None
-current_period = []
-
-for day in range(1, len(amounts)):
-    if amounts[day] > amounts[day - 1]:  # If amount increased from previous day
-        if current_trend == "increasing":
-            current_period.append(int(Cash_on_hand[day][0]))
-        else:
-            if current_period:
-                increasing_period.append(current_period)
-            current_trend = "increasing"
-            current_period = [int(Cash_on_hand[day][0])]
-    elif amounts[day] < amounts[day - 1]:  # If amount decreased from previous day
-        if current_trend == "decreasing":
-            current_period.append(int(Cash_on_hand[day][0]))
-        else:
-            if current_period:
-                decreasing_period.append(current_period)
-            current_trend = "decreasing"
-            current_period = [int(Cash_on_hand[day][0])]
-
-# Append the last period
-if current_trend == "increasing":
-    increasing_period.append(current_period)
-elif current_trend == "decreasing":
-    decreasing_period.append(current_period)
-
-# Output the results
-for period in increasing_period:
-    print(f"Day {period[0]} to {period[-1]}: Increase in cash surplus")
-
-for period in decreasing_period:
-    print(f"Day {period[0]} to {period[-1]}: Decrease in cash surplus")
-
-# Scenerio 1
-def max_increase_decrease_finder(Cash_on_hand):
-    greatest_cash_surplus_increase = 0
-    day_of_greatest_increase = 0
-    day_of_greatest_decrease = 0
-    greatest_cash_surplus_decrease = 0
-
+def find_deficits(Cash_on_hand):
+    deficits = []
     for day in range(1, len(Cash_on_hand)):
-        previous_cash_on_hand = Cash_on_hand[day - 1][1]
-        current_cash_on_hand = Cash_on_hand[day][1]
-        change_in_cash_on_hand = current_cash_on_hand - previous_cash_on_hand
+        previous_cash = Cash_on_hand[day - 1][1]
+        current_cash = Cash_on_hand[day][1]
+        deficit = previous_cash - current_cash
+
+        if deficit > current_cash:
+            day = int(Cash_on_hand[day][0])
+            deficits.append((day, deficit))
+
+    # for day, deficit in deficits:
+        print(f"[CASH DEFICIT] DAY: {day}, AMOUNT: {deficit}")
+
+find_deficits(Cash_on_hand)
+
+def bob(Cash_on_hand):
+    final_amount = []
+    for day in range(1, len(Cash_on_hand)):
+        previous_cash = Cash_on_hand[day - 1][1]
+        current_cash = Cash_on_hand[day][1]
+        amount = previous_cash - current_cash
+
+        if amount > 0:
+            day = int(Cash_on_hand[day][0])
+            final_amount.append((day, amount))
         
-        if change_in_cash_on_hand > greatest_cash_surplus_increase:
-            greatest_cash_surplus_increase = change_in_cash_on_hand
-            day_of_greatest_increase = int(Cash_on_hand[day][0])
-        
-        if change_in_cash_on_hand < greatest_cash_surplus_decrease:
-            greatest_cash_surplus_decrease = change_in_cash_on_hand
-            day_of_greatest_decrease = int(Cash_on_hand[day][0])
+        print(f"[CASH DEFICIT] DAY: {day}, AMOUNT: {amount}")
 
-    print(f"[HIGHEST CASH SURPLUS] Day: {day_of_greatest_increase}, AMOUNT: USD{greatest_cash_surplus_increase}")
-    print(f"[HIGHEST CASH DEFICIT] Day: {day_of_greatest_decrease}, AMOUNT: USD{greatest_cash_surplus_decrease}")
-    
+#         elif current_cash > amount:
+#             highest_amount = current_cash
+#             highest_amount_day = int(Cash_on_hand[day][0])
+#             print("Cash surplus is higher every day")
+#     # if highest_amount_day > 0:
+#             print(f"Highest cash amount: USD{highest_amount} on DAY: {highest_amount_day}")
 
-# Call the function to find and print the results
-max_increase_decrease_finder(Cash_on_hand)
-
-
-
-
-
-
+# bob(Cash_on_hand)
 
 
 

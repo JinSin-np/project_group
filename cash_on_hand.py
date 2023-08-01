cbccafe9fa9e05c630d6852480e5517afb923dfd
Tay Jia Wei <s10262468@connect.np.edu.sg>
2023-08-01 23:38:28 +0800

    finally finish

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -17,26 +17,50 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #and append the salesRecords list
         Cash_on_hand.append([row[0],int(row[1])])
 
-print(Cash_on_hand)
+# print(Cash_on_hand)
 
-def print_cash_deficit(data):
-    if len(data) < 2:
+# def print_cash_deficit(data):
+#     if len(data) < 2:
+#         return
+
+#     # Initialize prev_cash with the first day's cash amount
+#     day, prev_cash = data[0]
+
+#     for next_day, cash in data[1:]:
+#         if cash < prev_cash:
+#             deficit = prev_cash - cash
+#             print(f"[CASH DEFICIT] DAY: {next_day}, AMOUNT: {deficit}")
+#         prev_cash = cash
+#         # elif prev_cash > cash:
+#         #     print(f"CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+#             # print(f"[HIGHEST CASH SURPLUS] DAY: {}, AMOUNT: {}")
+
+def print_cash_changes(data):
+    if not data:
         return
 
-    # Initialize prev_cash with the first day's cash amount
+    has_decrease = False
+    max_increase_day = ""
+    max_increase_amount = 0
+
     day, prev_cash = data[0]
 
     for next_day, cash in data[1:]:
         if cash < prev_cash:
             deficit = prev_cash - cash
             print(f"[CASH DEFICIT] DAY: {next_day}, AMOUNT: {deficit}")
+            has_decrease = True
+        else:
+            increase = cash - prev_cash
+            if increase > max_increase_amount:
+                max_increase_day = next_day
+                max_increase_amount = increase
+
         prev_cash = cash
-        # elif prev_cash > cash:
-        #     print(f"CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
-            # print(f"[HIGHEST CASH SURPLUS] DAY: {}, AMOUNT: {}")
+
+    if not has_decrease and max_increase_day:
+        print("CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+        print(f"[MAX CASH INCREASE] DAY: {max_increase_day}, AMOUNT: {max_increase_amount}")
 
 
-
-
-print_cash_deficit(Cash_on_hand)
-
+print_cash_changes(Cash_on_hand)

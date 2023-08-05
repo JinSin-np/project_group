5ee365e7640528ae0462f4dee7d17ba2f1287538
WongsaDaonil <S10262620@connect.np.edu.sg>
2023-08-05 09:31:27 +0800

    edited

--- main_solution.py
+++ main_solution.py
@@ -21,17 +21,8 @@ def main():
 if __name__ == "__main__":
     main()
 
+import mod_1
+import mod_2
 
-# Manually type in the calculated output amount as a temporarily value for the variable "Answer_output" 
-Answer_output = [["EMP01", "7508.08","153","22","131","210.81","2044.81"],["EMP02","5876.89","138","17","121","70.15","1764.15"],["EMP03","9604.83","156","22","134","432.58","2308.58"]] 
-
-# name the text file as "paymentSummary.txt"
-with open("paymentSummary.txt", "w") as file: 
-    # adding title into the text file, using "\n" to move a new line to the next row 
-#     file.write("FashionStore Payment Summary\n============================\nEmployee ID, Total Sales, Total Hours, Break Hours, Total Hours Clocked, Commission, Total Pay\n") 
-    header = "FashionStore Payment Summary\n============================\nEmployee ID, Total Sales, Total Hours, Break Hours, Total Hours Clocked, Commission, Total Pay\n"
-    file.write(header)
-
-    # assigning the Answer_output values via factors 0 to 6, using "\n" to move a new line to the next row 
-    for factor in Answer_output:
-        file.write(f"{factor[0]},{factor[1]},{factor[2]},{factor[3]},{factor[4]},{factor[5]},{factor[6]}\n") 
+print(mod_1.underline("Hello World", "*"))
+print(mod_2.add(18,28))

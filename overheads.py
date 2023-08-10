d873c51ba385e4be78d7195c49c482bcac0eedde
JinSin-np <s10256144@connect.np.edu.sg>
2023-08-10 15:55:52 +0800

    moved csv files to separate folder and name changed to fit project brief

--- overheads.py
+++ overheads.py
@@ -6,7 +6,7 @@ def overheads_path_reader():
     No parameters required
     '''
 # create a file to csv file.
-    fp = Path.cwd()/"overheads.csv"
+    fp = Path.cwd()/"csv_reports"/"overheads.csv"
 
     # read the csv file to append profit and quantity from the csv.
     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
